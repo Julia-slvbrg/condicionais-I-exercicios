67eacb6fceccd5d459a17ba90de029d18068791c
Julia-slvbrg <julia.slvb@gmail.com>
2022-11-09 22:13:05 -0300

    Exercícios 1 e 2 completos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,15 @@
-const num = //seu numero aqui
+const num = 5
 
-if(){
-    console.log("sua mensagem aqui");
+if(num % 2 === 0){
+    console.log("A constante é um número par");
 }
+
+//se fosse para fazer um retorno do número não ser par:
+
+/* 
+if(num % 2 === 0){
+    console.log("A constante é um número par");
+}else{
+    console.log("A constante é um número ímpar")
+} 
+*/
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,27 @@
 let idade;
-let isMaiorIdade;
+let terminouEnsinoMedio;
 let isCursandoOutraFaculdade;
 
-if(){
+idade = 25
 
+if(idade >= 18){
+    console.log('A pessoa é maior de idade')
 }else{
-	
+	console.log('A pessoa é menor de idade')
+}
+
+terminouEnsinoMedio = true
+
+if(terminouEnsinoMedio == true){
+    console.log('A pessoa terminou o ensino médio')
+}else{
+    console.log('A pessoa não terminou o ensino médio')
+}
+
+isCursandoOutraFaculdade = false
+
+if(!isCursandoOutraFaculdade == true){
+    console.log('A pessoa não está cursando alguma faculdade')
+}else{
+    console.log('A pessoa está cursando alguma faculdade')
 }
